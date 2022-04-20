b03247cbf3cd99e70900687d0fe94b7add62a235
saraAdibi <sara.adibi2000@yahoo.com>
2022-04-20 11:08:13 +0430

    initial commit

---
+++ eshop_order/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from eshop_order.views import add_user_order, user_open_order
+
+urlpatterns = [
+    path('add-user-order', add_user_order),
+    path('open_order', user_open_order),
+]
---
+++ toplearn_eshop/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from eshop_sliders.models import Slider
+from eshop_settings.models import SiteSetting
+
+
+# header code behind
+def header(request, *args, **kwargs):
+    site_setting = SiteSetting.objects.first()
+    context = {
+        'setting': site_setting
+    }
+    return render(request, 'shared/Header.html', context)
+
+
+# footer code behind
+def footer(request, *args, **kwargs):
+    site_setting = SiteSetting.objects.first()
+    context = {
+        'setting': site_setting
+    }
+    return render(request, 'shared/Footer.html', context)
+
+
+# code behind
+def home_page(request):
+    sliders = Slider.objects.all()
+    context = {
+        'data': 'این سایت فروشگاهی با فریم ورک django نوشته شده',
+        'sliders': sliders
+    }
+    return render(request, 'home_page.html', context)
+
+
+def about_page(request):
+    site_setting = SiteSetting.objects.first()
+    context = {
+        'setting': site_setting
+    }
+    return render(request, 'about_page.html', context)
